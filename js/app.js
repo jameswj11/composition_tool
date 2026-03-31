70ea3f3b39412613af9a41343ccbaac741e55b80
James Johnson <jameswj11@Jamess-MacBook-Air.local>
2026-03-31 18:31:16 -0400

    adds vertical slice shifting

--- js/app.js
+++ js/app.js
@@ -24,13 +24,13 @@ async function handleFiles(event) {
 
 // converts uploaded file into image
 function loadFileAsImage(file) {
-    return new Promise ((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         const url = URL.createObjectURL(file);
         const img = new Image();
 
-        img.onload = ()=>{
+        img.onload = () => {
             resolve({
-                file, 
+                file,
                 url,
                 image: img,
                 width: img.naturalWidth,
@@ -38,7 +38,7 @@ function loadFileAsImage(file) {
             });
         };
 
-        img.onerror = ()=>{
+        img.onerror = () => {
             URL.revokeObjectURL(url);
             resolve(null);
         };
@@ -67,14 +67,14 @@ function mutateImage(source) {
     const image = source.image;
     const maxWidth = 2500;
     const maxHeight = 2500;
-    const scale = Math.min(maxWidth/image.naturalWidth,
-        maxHeight/image.naturalHeight, 1
+    const scale = Math.min(maxWidth / image.naturalWidth,
+        maxHeight / image.naturalHeight, 1
     );
     const width = Math.floor(image.naturalWidth * scale);
     const height = Math.floor(image.naturalHeight * scale);
     const newCanvas = document.createElement('canvas');
     const ctx = newCanvas.getContext('2d');
-    
+
     newCanvas.width = width;
     newCanvas.height = height;
 
@@ -84,8 +84,17 @@ function mutateImage(source) {
     newCanvas.style.height = 'auto';
 
     ctx.drawImage(image, 0, 0, width, height);
-    
-    erasePolygon(ctx, width, height)
+
+    // here I can choose a random percentage where it won't happen
+
+    for (let i = 0; i < random(1, 5); i++) {
+        erasePolygon(ctx, width, height)
+    };
+    if (Math.random() < 0.7) {
+        shiftSlices(ctx, width, height);
+        shiftSlices(ctx, width, height)
+    };
+
 
     return newCanvas;
 };
@@ -98,8 +107,23 @@ function erasePolygon(ctx, width, height) {
     ctx.globalCompositeOperation = 'destination-out';
 
     // chooses a random location to start operation
-    const centerX = random(width * 0.2, width * 0.8);
-    const centerY = random(height * 0.2, height * 0.8);
+    let centerX;
+    let centerY;
+
+    if (Math.random() < 0.35) {
+        centerX = random(width * 0.25, width * 0.75);
+        centerY = random(height * 0.25, height * 0.75);
+    } else {
+        centerX =
+            Math.random() > 0.5
+                ? random(width * 0.05, width * 0.3)
+                : random(width * 0.7, width * 0.95);
+
+        centerY =
+            Math.random() > 0.5
+                ? random(height * 0.05, height * 0.3)
+                : random(height * 0.7, height * 0.95);
+    }
 
     // set size of polygon
     const minDim = Math.min(width, height);
@@ -130,6 +154,60 @@ function erasePolygon(ctx, width, height) {
     ctx.closePath();
     ctx.fill();
     ctx.restore();
+};
+
+function shiftSlices(ctx, width, height) {
+    // creates temporary canvas and copies original into it for mutation
+    const tempCanvas = document.createElement('canvas');
+    const tempCtx = tempCanvas.getContext('2d');
+
+    tempCanvas.width = width;
+    tempCanvas.height = height;
+    
+    tempCtx.drawImage(ctx.canvas, 0, 0);
+
+    const horizontal = Math.random() < 0.5;
+
+    // create slices
+    if (horizontal) {
+        let y = 0;
+
+        while (y < height) {
+            let sliceHeight = Math.floor(random(height * 0.06, height * 0.22));
+            sliceHeight = Math.min(sliceHeight, height - y);
+
+            let shiftX;
+            if (Math.random() < 0.25) {
+                shiftX = 0;
+            } else if (Math.random() < 0.75) {
+                shiftX = random(-25, 25);
+            } else {
+                shiftX = random(-60, 60);
+            }
+            ctx.drawImage(tempCanvas, 0, y, width, sliceHeight, shiftX, y, width, sliceHeight);
+
+            y += sliceHeight;
+        }
+    } else {
+        let x = 0;
+
+        while (x < width) {
+            let sliceWidth = Math.floor(random(width * 0.06, width * 0.22));
+            sliceWidth = Math.min(sliceWidth, width - x);
+            
+            let shiftY;
+            if (Math.random() < 0.25) {
+                shiftY = 0;
+            } else if (Math.random() < 0.75) {
+                shiftY = random(-25, 25);
+            } else {
+                shiftY = random(-60, 60)
+            }
+
+            ctx.drawImage(tempCanvas, x, 0, sliceWidth, height, x, shiftY, sliceWidth, height);
+            x += sliceWidth;
+        };
+    };
 };
 
 // random helper
